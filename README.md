9cb72ea8ba9b125c829bd17ba07ba872f5b15373
Lena <me@lena.place>
2023-07-31 11:06:34 +0200

    cargo fmt and make readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# oorah-broadcaster
+Service to broadcast notifications over websockets for canvas game factions that use [TemplateManager](https://github.com/osuplace/templateManager), rust rewrite
+
+The original typescript version is [here](https://github.com/april83c/broadcaster)
+
+## Installation
+
+TBD
+
+## Usage
+
+Fully conforms to the [API of the original broadcaster](https://github.com/april83c/broadcaster/blob/main/API.md)
+
+## Panel
+
+No GUI (yet?), can use the original broadcaster's
+
+## Logging
+
+Notifications or actions aren't logged to stdout or a file, but support will be added
+
+## Development setup
+
+- Have a rust installation from [rustup](https://rustup.rs/)
+- Clone the repo
+- `cargo run`
--- src/websockets.rs
+++ src/websockets.rs
@@ -29,7 +29,7 @@ async fn handle_socket(mut socket: WebSocket, who: SocketAddr, state: Arc<AppSta
 
     /*
         TODO: investigate high memory usage
-        
+
         After a lot of websocket connections (~20K) get opened, the memory usage increases
         to about ~200MiB (as it should), but when those connections close, the OS doesn't
         reclaim that memory.
@@ -49,15 +49,15 @@ async fn handle_socket(mut socket: WebSocket, who: SocketAddr, state: Arc<AppSta
                     Maybe the tungstenite-rs will support Bytes in the future (e.g. Arc<[u8]>)
                     Or just Arc<str> or something, but this clone alone takes half of the CPU time
                     and i suspect the rest of the CPU time is taken by the internal buffer cloning
-                    
+
                     Either way, the memory clones are what is taking the most CPU time,
                     and removing them will turn down the CPU time spent per websocket connection,
                     allowing for more and more connections at a time.
 
                     In any case, these are all CPU *spikes* that happen on every broadcast message,
                     and thus even if we reach 100% CPU time, it'll only increase the latency slightly,
-                    and it won't completely tank performance. 
-                    
+                    and it won't completely tank performance.
+
                     This happens at about 50K open websockets on a Ryzen 5 3600, so pretty decent.
                 */
                 if let Err(_) = socket.send(Message::Text(msg.to_string())).await {
